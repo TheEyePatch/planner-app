f04de012c194603875cb803bf91ce5ec4ca21bb9
Leif Bersamina <leifbersamina@gmail.com>
2021-05-29 18:24:03 +0800

    apply more styles

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,16 +1,23 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './app/views/**/*.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       'rubik':['Rubik', 'sans-serif'],
       'big-shoulders':['Big Shoulders Display', 'cursive']
     },
+    extend:{
+      animation:{
+        bounce: 'bounce 2.5s infinite'
+      }
+    }
   },
   variants: {
-    extend: {
-      animation: ['motion-reduce']
-    },
+  
   },
   plugins: [],
 }
