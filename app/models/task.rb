aab7608d81efcf02f98eeede7525afeaf11a5b90
Leif Bersamina <leifbersamina@gmail.com>
2021-06-02 15:56:41 +0800

    add uniqueness of category per user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ApplicationRecord
     belongs_to :category
-
     validates :title, presence: true
     validates :description, presence:true, length: {minimum: 10}
     validates :deadline, presence:true
